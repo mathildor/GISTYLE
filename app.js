529aada8b6724c412fafbb5fce36b4624d35721b
Mathilde <mathilde.orstavik@gmail.com>
2016-02-17 14:56:07 +0100

    all new

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
-var express = require('express'),
-    path = require('path');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-//create our express app
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
-//add some standard express middleware
-app.configure(function() {
-    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.static('static'));
-});
-
-//defining routes
-app.get('/', function(req, res) { //get request, '/' is the root of the domain (what?)
-    //res.send('hello world');
-    res.render('index');
-});
-
-//have our app listen on port 3000
-app.listen(3000);
-console.log('Your app is now running at: http://127.0.0.1:3000/');
-
-//setup our app to use handlebars.js for templating
-app.set('view engine', 'hbs');
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
