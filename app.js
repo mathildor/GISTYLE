322f55e7d529869ee79f6d4acbca45bea6f53d46
Mathilde <mathilde.orstavik@gmail.com>
2016-02-23 11:03:50 +0100

    removing cinflicting code

--- app.js
+++ app.js
@@ -63,33 +63,7 @@ module.exports = app;
 
 
 //FOR TESTING:
-var port = process.env.PORT || 8080;        // set our port
-
-
-//ROUTES FOR API:
-//=========================================================================================================
-
-
-var router=express.Router(); //Get instance of express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
-
+//var port = process.env.PORT || 8080;        // set our port
 
 
 //CONNECT TO DATABASE!
