997b8eb7defb3cbf23ec8527886a6bcde2d761fc
Mathilde <mathilde.orstavik@gmail.com>
2016-02-23 10:59:42 +0100

    api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var app = express(); //DEFINE THE APP
 
 
