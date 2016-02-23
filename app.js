a82a11202076f62164e2a8260c17f8fbfc99582f
Mathilde <mathilde.orstavik@gmail.com>
2016-02-23 11:16:42 +0100

    removing body parser

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 var app = express(); //DEFINE THE APP
 
